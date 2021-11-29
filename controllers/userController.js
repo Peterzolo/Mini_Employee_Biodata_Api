d61b7cff50a36780863a0752ad368ac934084e52
Peter Solomon <petersolomon704@gmail.com>
2021-11-29 14:00:40 +0100

    Added user login controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -113,4 +113,49 @@ exports.userRegister = async(req, res) =>{
         }
       };
       
+      exports.userLoginController = (req, res) => {
+        const { email, password } = req.body;
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          const firstError = errors.array().map(error => error.msg)[0];
+          return res.status(422).json({
+            errors: firstError
+          });
+        } else {
+          User.findOne({
+            email
+          }).exec((err, user) => {
+            if (err || !user) {
+              return res.status(400).json({
+                errors: 'User with that email does not exist. Please signup'
+              });
+            }
+            if (!user.authenticate(password)) {
+              return res.status(400).json({
+                errors: 'Email and password do not match'
+              });
+            }
+            const token = jwt.sign(
+              {
+                _id: user._id
+              },
+              process.env.JWT_ACCESS_TOKEN,
+              {
+                expiresIn: '1d'
+              }
+            );
+            const { _id, name, email, role } = user;
+      
+            return res.json({
+              token,
+              user: {
+                _id,
+                name,
+                email,
+                role
+              }
+            });
+          });
+        }
+      };
 
--- routes/user.js
+++ routes/user.js
@@ -5,11 +5,12 @@ const express = require('express');
 const router = express.Router();
 
 
-const { userRegister, userRegisterEmailActivationController } = require('../controllers/userController');
+const { userRegister, userRegisterEmailActivationController, userLoginController } = require('../controllers/userController');
 
 
 router.route('/register').post(userRegister)
 router.route('/register/activation').post(userRegisterEmailActivationController)
+router.route('/login').post(userLoginController)
 
 
 
